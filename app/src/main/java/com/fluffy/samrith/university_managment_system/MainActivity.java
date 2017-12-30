c506e43b2ba3c556de1d328ff740ba7b21b6faba
samrith <samrith.yoeun@gmail.com>
2017-12-30 04:26:25 +0200

    created login screen

---
+++ app/src/main/java/com/fluffy/samrith/university_managment_system/MainActivity.java
@@ -0,0 +1,32 @@
+package com.fluffy.samrith.university_managment_system;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    EditText username;
+    EditText password;
+    Button login;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        username = (EditText)findViewById(R.id.username);
+        password = (EditText)findViewById(R.id.password);
+        login = (Button)findViewById(R.id.login);
+
+        login.setOnClickListener(view->{
+
+        });
+
+    }
+
+    public void signup(View v){
+        Toast.makeText(this, "You need to process some paper work to get an account", Toast.LENGTH_SHORT).show();
+    }
+}
